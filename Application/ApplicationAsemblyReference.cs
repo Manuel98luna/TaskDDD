994142599faf5bd5c4c6c9b3488c20eea0e305b7
Manuel98luna01~ <jmlzelaya22@gmail.com>
2023-07-01 00:02:18 -0600

    comple

--- Application/ApplicationAsemblyReference.cs
+++ Application/ApplicationAsemblyReference.cs
@@ -3,6 +3,5 @@ using System.Reflection;
 namespace Application;
 
 public class ApplicationAsemblyReference
-{
-    internal static readonly Assembly Assembly = typeof(ApplicationAsemblyReference).Assembly;
+{    internal static readonly Assembly Assembly = typeof(ApplicationAsemblyReference).Assembly;
 }
